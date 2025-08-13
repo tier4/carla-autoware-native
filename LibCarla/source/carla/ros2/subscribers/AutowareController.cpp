a085920a72fa635a611b4ba3caf4e0a6811df68c
Mateusz Palczuk <mateusz.palczuk@robotec.ai>
2025-08-13 16:23:03 +0200

    Use minimal AutowareController (only Ackermann Control) and use received input to control vehicle in Carla
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk@robotec.ai>

--- LibCarla/source/carla/ros2/subscribers/AutowareController.cpp
+++ LibCarla/source/carla/ros2/subscribers/AutowareController.cpp
@@ -7,6 +7,7 @@
 namespace carla {
 namespace ros2 {
 
+// Define subscribers
 class AutowareControlSubscriber
 : public AutowareSubscriber<autoware_control_msgs::msg::Control, autoware_control_msgs::msg::ControlPubSubType>
 {
@@ -21,35 +22,45 @@ class AutowareController::Implementation {
 public:
   Implementation() = default;
   AutowareControlSubscriber _control_subscriber{};
+  void* _vehicle;
 };
 
-AutowareController::AutowareController(const CarlaSubscriber::DomainId domain_id)
+AutowareController::AutowareController(void* actor, const CarlaSubscriber::DomainId domain_id)
 : _impl(std::make_shared<Implementation>())
 {
   AutowareSubscriberConfig config;
   config.domain_id = domain_id;
   _impl->_control_subscriber.Init(config);
+  _impl->_vehicle = actor;
 }
 
 bool AutowareController::HasNewControl() const {
   return _impl->_control_subscriber.HasNewMessage();
 }
 
-
 VehicleAckermannControl AutowareController::GetControl() {
   VehicleAckermannControl control_out;
-
   const auto control_in = _impl->_control_subscriber.GetMessage();
-  control_out.speed = control_in.longitudinal().velocity();
-  control_out.acceleration = control_in.longitudinal().acceleration();
-  control_out.jerk = control_in.longitudinal().jerk();
 
-  control_out.steer = control_in.lateral().steering_tire_angle();
+  control_out.speed = control_in.longitudinal().velocity();
+  if (control_in.longitudinal().is_defined_acceleration()) {
+    control_out.acceleration = control_in.longitudinal().acceleration();
+  }
+  if (control_in.longitudinal().is_defined_jerk()) {
+    control_out.jerk = control_in.longitudinal().jerk();
+  }
+
+  /// @note Set lateral negative, because Carla treats positive as right and Autoware expects positive to represent left (all when moving forward)
+  control_out.steer = -control_in.lateral().steering_tire_angle();
   if (control_in.lateral().is_defined_steering_tire_rotation_rate()) {
-    control_out.steer_speed = control_in.lateral().steering_tire_rotation_rate();
+    control_out.steer_speed = -control_in.lateral().steering_tire_rotation_rate();
   }
 
   return control_out;
+}
+
+void* AutowareController::GetVehicle() {
+  return _impl->_vehicle;
 }
 
 }  // namespace ros2
