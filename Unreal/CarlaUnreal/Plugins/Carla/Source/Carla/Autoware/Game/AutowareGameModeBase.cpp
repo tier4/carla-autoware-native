d0c486611eb12758e0be3a44c783db4cad08bdbc
Wojciech Czerski <wojciech.czerski@robotec.ai>
2025-10-23 14:57:27 +0200

    Create derivered AutowareGnssSensor and GameModeBase
    Signed-off-by: Wojciech Czerski <wojciech.czerski@robotec.ai>

---
+++ Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Autoware/Game/AutowareGameModeBase.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2024 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB). This work is licensed under the terms of the MIT license. For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#include "AutowareGameModeBase.h"
+
+#include "AutowareWorldSettings.h"
+
+AAutowareGameModeBase::AAutowareGameModeBase(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void AAutowareGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+}
+
+void AAutowareGameModeBase::LoadGeoReference()
+{
+	auto* WS = Cast<AAutowareWorldSettings>(GetWorld()->GetWorldSettings());
+
+	if (!IsValid(WS))
+	{
+		Super::LoadGeoReference(); // Fallback to CarlaGameMode::LoadGeoReference default
+		return;
+	}
+	
+	UE_LOG(LogCarla, Warning, TEXT("Autoware Settings fetch succeded."));
+	auto* Data = WS->MgrsDataAssetSoftPtr.LoadSynchronous();
+
+	if (!IsValid(Data))
+	{
+		return;
+	}
+	
+	carla::geom::GeoLocation GeoReference
+	(
+	  Data->WorldOriginGeoCoordinate.Latitude,
+	  Data->WorldOriginGeoCoordinate.Longitude,
+	  Data->WorldOriginGeoCoordinate.Altitude
+	);
+	Episode->MapGeoReference = GeoReference;
+
+	UE_LOG(LogCarla, Warning, TEXT("MGRS Offset loaded successfuly."));
+	StoreSpawnPoints();
+}
