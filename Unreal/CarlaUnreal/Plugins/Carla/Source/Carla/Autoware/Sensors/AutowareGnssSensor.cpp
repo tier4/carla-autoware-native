tier4/carla-autoware-native

commit 8791ce07b48c9f000feee32b7236715b029d79d0

Create/fix custom AutowareGnssSensor. Read data from MGRS DataAsset and fetch geo reference from autoware game mode -> episode | Create custom function library to set Autoware Gnss sensor definition. | Update ros2 with autoware gnss process data | AutowareGnssPublisher get mgrs offset from passed value
Signed-off-by: wczerski <wojciech.czerski@robotec.ai>

Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Autoware/Sensors/AutowareGnssSensor.cpp
@@ -3,15 +3,19 @@
 
 #include "AutowareGnssSensor.h"
 
+#include <carla/rpc/Vector3D.h>
+
 #include "Actor/ActorBlueprintFunctionLibrary.h"
 #include "Autoware/Game/AutowareWorldSettings.h"
+#include "Game/CarlaStatics.h"
 
 
 // Sets default values
 AAutowareGnssSensor::AAutowareGnssSensor(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	SetNoiseErrorActive(false);
+	PrimaryActorTick.bCanEverTick = true;
+	RandomEngine = CreateDefaultSubobject<URandomEngine>(TEXT("RandomEngine"));
 }
 
 FActorDefinition AAutowareGnssSensor::GetSensorDefinition()
@@ -24,40 +28,72 @@ FActorDefinition AAutowareGnssSensor::GetSensorDefinition()
 void AAutowareGnssSensor::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	// Load mgrs data from active level
 	LoadMgrsData();
+	
+	const UCarlaEpisode* episode = UCarlaStatics::GetCurrentEpisode(GetWorld());
+	CurrentGeoReference = episode->GetGeoReference(); // Can be directly set inside gnss sensor, but carla does it in game mode base and attaches it into episode
 }
 
 void AAutowareGnssSensor::PostPhysTick(UWorld* World, ELevelTick TickType, float DeltaSeconds)
 {
-	Super::PostPhysTick(World, TickType, DeltaSeconds);
-}
-
-carla::geom::GeoLocation AAutowareGnssSensor::ComputeGeoLocation() const
-{
-	if (!MgrsDataAsset)
-	{
-		// Fallback to parent behavior if asset missing
-		return Super::ComputeGeoLocation();
-	}
+	TRACE_CPUPROFILER_EVENT_SCOPE(AAutowareGnssSensor::PostPhysTick);
 
 	FVector ActorLocation = GetActorLocation();
-	ActorLocation += MgrsDataAsset->MgrsOffsetPosition;
-	const auto& OriginGeo = MgrsDataAsset->WorldOriginGeoCoordinate;
+	carla::geom::Location Location = ActorLocation;
+	carla::geom::GeoLocation CurrentLocation = CurrentGeoReference.Transform(Location);
+	
+	LatitudeValue = CurrentLocation.latitude;
+	LongitudeValue = CurrentLocation.longitude;
+	AltitudeValue = CurrentLocation.altitude;
+	
+	if (bApplyNoiseError)
+	{
+		// Compute the noise for the sensor
+		const float LatError = RandomEngine->GetNormalDistribution(0.0f, LatitudeDeviation);
+		const float LonError = RandomEngine->GetNormalDistribution(0.0f, LongitudeDeviation);
+		const float AltError = RandomEngine->GetNormalDistribution(0.0f, AltitudeDeviation);
 
-	carla::geom::Location Location{
-		static_cast<float>(ActorLocation.X),
-		static_cast<float>(ActorLocation.Y),
-		static_cast<float>(ActorLocation.Z)
-	};
+		// Apply the noise to the sensor
+		LatitudeValue += LatitudeBias + LatError;
+		LongitudeValue += LongitudeBias + LonError;
+		AltitudeValue += AltitudeBias + AltError;
+	}
+	
+	auto DataStream = GetDataStream(*this);
 
-	carla::geom::GeoLocation GeoOrigin(OriginGeo.Latitude, OriginGeo.Longitude, OriginGeo.Altitude);
-
-	carla::geom::GeoLocation Result;
-	Result.latitude  = OriginGeo.Latitude  + Location.y * 1e-5;
-	Result.longitude = OriginGeo.Longitude + Location.x * 1e-5;
-	Result.altitude  = OriginGeo.Altitude  + Location.z;
-
-	return Result;
+	// ROS2
+	#if defined(WITH_ROS2)
+	auto ROS2 = carla::ros2::ROS2::GetInstance();
+	if (ROS2->IsEnabled())
+	{
+		TRACE_CPUPROFILER_EVENT_SCOPE_STR("ROS2 Send");
+		auto StreamId = carla::streaming::detail::token_type(GetToken()).get_stream_id();
+		AActor* ParentActor = GetAttachParentActor();
+		const FTransform sensor_world_transform = GetActorTransform();
+		double mgrs_offset_position[3] = {
+			static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.X),
+			static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.Y),
+			static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.Z),
+		};
+		
+		if (ParentActor)
+		{
+			FTransform LocalTransformRelativeToParent = GetActorTransform().GetRelativeTransform(ParentActor->GetActorTransform());
+			ROS2->ProcessDataFromAutowareGNSS(DataStream.GetSensorType(), StreamId, LocalTransformRelativeToParent, carla::geom::GeoLocation{LatitudeValue, LongitudeValue, AltitudeValue}, sensor_world_transform, mgrs_offset_position, this);
+		}
+		else
+		{
+			ROS2->ProcessDataFromAutowareGNSS(DataStream.GetSensorType(), StreamId, DataStream.GetSensorTransform(), carla::geom::GeoLocation{LatitudeValue, LongitudeValue, AltitudeValue}, sensor_world_transform, mgrs_offset_position, this);
+		}
+	}
+	#endif
+	
+	{
+		TRACE_CPUPROFILER_EVENT_SCOPE_STR("AAutowareGnssSensor Stream Send");
+		DataStream.SerializeAndSend(*this, carla::geom::GeoLocation{LatitudeValue, LongitudeValue, AltitudeValue});
+	}
 }
 
 void AAutowareGnssSensor::LoadMgrsData()
@@ -83,4 +119,91 @@ void AAutowareGnssSensor::LoadMgrsData()
 
 		MgrsDataAsset = Data;
 	}
+}
+
+void AAutowareGnssSensor::Set(const FActorDescription& ActorDescription)
+{
+	Super::Set(ActorDescription);
+	UActorBlueprintFunctionLibrary::SetAutowareGnss(ActorDescription, this);
+}
+
+void AAutowareGnssSensor::SetLatitudeDeviation(float Value)
+{
+	LatitudeDeviation = Value;
+}
+
+void AAutowareGnssSensor::SetLongitudeDeviation(float Value)
+{
+	LongitudeDeviation = Value;
+}
+
+void AAutowareGnssSensor::SetAltitudeDeviation(float Value)
+{
+	AltitudeDeviation = Value;
+}
+
+void AAutowareGnssSensor::SetLatitudeBias(float Value)
+{
+	LatitudeBias = Value;
+}
+
+void AAutowareGnssSensor::SetLongitudeBias(float Value)
+{
+	LongitudeBias = Value;
+}
+
+void AAutowareGnssSensor::SetAltitudeBias(float Value)
+{
+	AltitudeBias = Value;
+}
+
+float AAutowareGnssSensor::GetLatitudeDeviation() const
+{
+	return LatitudeDeviation;
+}
+float AAutowareGnssSensor::GetLongitudeDeviation() const
+{
+	return LongitudeDeviation;
+}
+float AAutowareGnssSensor::GetAltitudeDeviation() const
+{
+	return AltitudeDeviation;
+}
+
+float AAutowareGnssSensor::GetLatitudeBias() const
+{
+	return LatitudeBias;
+}
+float AAutowareGnssSensor::GetLongitudeBias() const
+{
+	return LongitudeBias;
+}
+float AAutowareGnssSensor::GetAltitudeBias() const
+{
+	return AltitudeBias;
+}
+
+double AAutowareGnssSensor::GetLatitudeValue() const
+{
+	return LatitudeValue;
+}
+
+double AAutowareGnssSensor::GetLongitudeValue() const
+{
+	return LongitudeValue;
+}
+
+double AAutowareGnssSensor::GetAltitudeValue() const
+{
+	return AltitudeValue;
+}
+
+bool AAutowareGnssSensor::IsNoiseErrorEnabled() const
+{
+	return bApplyNoiseError;
+}
+
+void AAutowareGnssSensor::SetNoiseErrorActive(const bool bEnabled)
+{
+	bApplyNoiseError = bEnabled;
 }

LibCarla/source/carla/ros2/ROS2.cpp
@@ -881,6 +881,29 @@ void ROS2::ProcessDataFromGNSS(
   }
 }
 
+void ROS2::ProcessDataFromAutowareGNSS(
+  uint64_t sensor_type,
+  carla::streaming::detail::stream_id_type stream_id,
+  const carla::geom::Transform sensor_transform,
+  const carla::geom::GeoLocation &data,
+  const carla::geom::Transform &sensor_world_transform,
+  const double mgrs_offset_position[3],
+  void *actor) {
+  log_info("Sensor GnssSensor to ROS data: frame.", _frame, "sensor.", sensor_type, "stream.", stream_id, "geo.", data.latitude, data.longitude, data.altitude);
+  auto sensors = GetOrCreateSensor(ESensors::GnssSensor, stream_id, actor);
+  if (sensors.first) {
+    if (const auto autoware_publisher = std::dynamic_pointer_cast<AutowareGNSSPublisher>(sensors.first)) {
+      autoware_publisher->SetData(_seconds, _nanoseconds, (const float*)&sensor_world_transform.location, (const float*)&sensor_world_transform.rotation, mgrs_offset_position);
+      autoware_publisher->Publish();
+    }
+  }
+  if (sensors.second && _publish_tf) {
+    std::shared_ptr<CarlaTransformPublisher> publisher = std::dynamic_pointer_cast<CarlaTransformPublisher>(sensors.second);
+    publisher->SetData(_seconds, _nanoseconds, (const float*)&sensor_transform.location, (const float*)&sensor_transform.rotation);
+    publisher->Publish();
+  }
+}
+
 void ROS2::ProcessDataFromIMU(
     uint64_t sensor_type,
     carla::streaming::detail::stream_id_type stream_id,

LibCarla/source/carla/ros2/ROS2.h
@@ -124,6 +124,14 @@ class ROS2
       const carla::geom::GeoLocation &data,
       const carla::geom::Transform &sensor_world_transform,
       void *actor = nullptr);
+  void ProcessDataFromAutowareGNSS(
+      uint64_t sensor_type,
+      carla::streaming::detail::stream_id_type stream_id,
+      const carla::geom::Transform sensor_transform,
+      const carla::geom::GeoLocation &data,
+      const carla::geom::Transform &sensor_world_transform,
+      const double mgrs_offset_position[3],
+      void *actor = nullptr);
   void ProcessDataFromIMU(
       uint64_t sensor_type,
       carla::streaming::detail::stream_id_type stream_id,

LibCarla/source/carla/ros2/publishers/AutowareGNSSPublisher.cpp
@@ -52,54 +52,117 @@ bool AutowareGNSSPublisher::Publish() {
     _impl->_pose_with_covariance_publisher.Publish();
 }
 
-void AutowareGNSSPublisher::SetData(int32_t seconds, uint32_t nanoseconds, const float* translation, const float* rotation) {
-  geometry_msgs::msg::Pose pose;
+void AutowareGNSSPublisher::SetData(int32_t seconds, uint32_t nanoseconds, const float* translation, const float* rotation, const double* mgrs_offset_position)
+{
+	geometry_msgs::msg::Pose pose;
 
-  /// @note: Copied from CarlaTransformPublisher
-  const float tx = *translation++;
-  const float ty = *translation++;
-  const float tz = *translation++;
+	/// @note: Copied from CarlaTransformPublisher
+	const float tx = *translation++;
+	const float ty = *translation++;
+	const float tz = *translation++;
 
-  const float rx = ((*rotation++) * -1.0f) * (M_PIf32 / 180.0f);
-  const float ry = ((*rotation++) * -1.0f) * (M_PIf32 / 180.0f);
-  const float rz = *rotation++ * (M_PIf32 / 180.0f);
+	const float rx = ((*rotation++) * -1.0f) * (M_PIf32 / 180.0f);
+	const float ry = ((*rotation++) * -1.0f) * (M_PIf32 / 180.0f);
+	const float rz = *rotation++ * (M_PIf32 / 180.0f);
 
-  const float cr = cosf(rz * 0.5f);
-  const float sr = sinf(rz * 0.5f);
-  const float cp = cosf(rx * 0.5f);
-  const float sp = sinf(rx * 0.5f);
-  const float cy = cosf(ry * 0.5f);
-  const float sy = sinf(ry * 0.5f);
+	const float cr = cosf(rz * 0.5f);
+	const float sr = sinf(rz * 0.5f);
+	const float cp = cosf(rx * 0.5f);
+	const float sp = sinf(rx * 0.5f);
+	const float cy = cosf(ry * 0.5f);
+	const float sy = sinf(ry * 0.5f);
 
-  // TODO: Verify whether this data layout is correct
-  pose.position().x(tx);
-  pose.position().y(-ty);
-  pose.position().z(tz);
+	// TODO: Verify whether this data layout is correct
 
-  pose.orientation().w(cr * cp * cy + sr * sp * sy);
-  pose.orientation().x(sr * cp * cy - cr * sp * sy);
-  pose.orientation().y(cr * sp * cy + sr * cp * sy);
-  pose.orientation().z(cr * cp * sy - sr * sp * cy);
+	const double mgrs_x = mgrs_offset_position[0];
+	const double mgrs_y = mgrs_offset_position[1];
+	const double mgrs_z = mgrs_offset_position[2];
 
-  _impl->_pose_publisher.SetData(pose);
+	pose.position().x(tx + mgrs_x);
+	pose.position().y(-ty + mgrs_y);  // note original y was negated
+	pose.position().z(tz + mgrs_z);
 
-  geometry_msgs::msg::PoseWithCovarianceStamped pose_with_covariance;
+	pose.orientation().w(cr * cp * cy + sr * sp * sy);
+	pose.orientation().x(sr * cp * cy - cr * sp * sy);
+	pose.orientation().y(cr * sp * cy + sr * cp * sy);
+	pose.orientation().z(cr * cp * sy - sr * sp * cy);
 
-  builtin_interfaces::msg::Time time;
-  time.sec(seconds);
-  time.nanosec(nanoseconds);
+	_impl->_pose_publisher.SetData(pose);
 
-  std_msgs::msg::Header header;
-  header.stamp(std::move(time));
-  header.frame_id(_impl->_pose_with_covariance_publisher.frame_id());
+	geometry_msgs::msg::PoseWithCovarianceStamped pose_with_covariance;
 
-  std::array<double, 36> covariance{};  // TODO: Add some covariance matrix
+	builtin_interfaces::msg::Time time;
+	time.sec(seconds);
+	time.nanosec(nanoseconds);
 
-  pose_with_covariance.header(std::move(header));
-  pose_with_covariance.pose().pose(std::move(pose));
-  pose_with_covariance.pose().covariance(std::move(covariance));
+	std_msgs::msg::Header header;
+	header.stamp(std::move(time));
+	header.frame_id(_impl->_pose_with_covariance_publisher.frame_id());
 
-  _impl->_pose_with_covariance_publisher.SetData(pose_with_covariance);
+	std::array<double, 36> covariance{};  // TODO: Add some covariance matrix
+
+	pose_with_covariance.header(std::move(header));
+	pose_with_covariance.pose().pose(std::move(pose));
+	pose_with_covariance.pose().covariance(std::move(covariance));
+
+	_impl->_pose_with_covariance_publisher.SetData(pose_with_covariance);
+}
+
+void AutowareGNSSPublisher::SetData(int32_t seconds, uint32_t nanoseconds, const float* translation, const float* rotation)
+{
+	geometry_msgs::msg::Pose pose;
+
+	/// @note: Copied from CarlaTransformPublisher
+	const float tx = *translation++;
+	const float ty = *translation++;
+	const float tz = *translation++;
+
+	const float rx = ((*rotation++) * -1.0f) * (M_PIf32 / 180.0f);
+	const float ry = ((*rotation++) * -1.0f) * (M_PIf32 / 180.0f);
+	const float rz = *rotation++ * (M_PIf32 / 180.0f);
+
+	const float cr = cosf(rz * 0.5f);
+	const float sr = sinf(rz * 0.5f);
+	const float cp = cosf(rx * 0.5f);
+	const float sp = sinf(rx * 0.5f);
+	const float cy = cosf(ry * 0.5f);
+	const float sy = sinf(ry * 0.5f);
+
+	// TODO: Verify whether this data layout is correct
+
+	// Apply MGRS offsets
+	const double mgrs_x = 81655.73;
+	const double mgrs_y = 50137.43;
+
+	// Publish pose with MGRS offset
+	pose.position().x(tx + mgrs_x);
+	pose.position().y(-ty + mgrs_y);  // note original y was negated
+	pose.position().z(tz);
+
+	pose.orientation().w(cr * cp * cy + sr * sp * sy);
+	pose.orientation().x(sr * cp * cy - cr * sp * sy);
+	pose.orientation().y(cr * sp * cy + sr * cp * sy);
+	pose.orientation().z(cr * cp * sy - sr * sp * cy);
+
+	_impl->_pose_publisher.SetData(pose);
+
+	geometry_msgs::msg::PoseWithCovarianceStamped pose_with_covariance;
+
+	builtin_interfaces::msg::Time time;
+	time.sec(seconds);
+	time.nanosec(nanoseconds);
+
+	std_msgs::msg::Header header;
+	header.stamp(std::move(time));
+	header.frame_id(_impl->_pose_with_covariance_publisher.frame_id());
+
+	std::array<double, 36> covariance{};  // TODO: Add some covariance matrix
+
+	pose_with_covariance.header(std::move(header));
+	pose_with_covariance.pose().pose(std::move(pose));
+	pose_with_covariance.pose().covariance(std::move(covariance));
+
+	_impl->_pose_with_covariance_publisher.SetData(pose_with_covariance);
 }
 
 AutowareGNSSPublisher::AutowareGNSSPublisher(const char* ros_name, const char* parent, const char* ros_topic_name) :

LibCarla/source/carla/ros2/publishers/AutowareGNSSPublisher.h
@@ -27,6 +27,7 @@ class AutowareGNSSPublisher : public CarlaPublisher {
     bool Init(const TopicConfig& pose_config, const TopicConfig& pose_config_with_covariance_stamped);
     bool Publish();
     void SetData(int32_t seconds, uint32_t nanoseconds, const float* position, const float* orientation);
+    void SetData(int32_t seconds, uint32_t nanoseconds, const float* position, const float* orientation, const double* mgrs_offset_position);
     const char* type() const override { return "Autoware GNSS"; }
 
   private:

Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.cpp
@@ -1906,6 +1906,35 @@ void UActorBlueprintFunctionLibrary::SetGnss(
       RetrieveActorAttributeToFloat("noise_alt_bias", Description.Variations, 0.0f));
 }
 
+void UActorBlueprintFunctionLibrary::SetAutowareGnss(
+	const FActorDescription &Description,
+	AAutowareGnssSensor *Gnss)
+{
+	CARLA_ABFL_CHECK_ACTOR(Gnss);
+	if (Description.Variations.Contains("noise_seed"))
+	{
+		Gnss->SetSeed(
+		  RetrieveActorAttributeToInt("noise_seed", Description.Variations, 0));
+	}
+	else
+	{
+		Gnss->SetSeed(Gnss->GetRandomEngine()->GenerateRandomSeed());
+	}
+
+	Gnss->SetLatitudeDeviation(
+		RetrieveActorAttributeToFloat("noise_lat_stddev", Description.Variations, 0.0f));
+	Gnss->SetLongitudeDeviation(
+		RetrieveActorAttributeToFloat("noise_lon_stddev", Description.Variations, 0.0f));
+	Gnss->SetAltitudeDeviation(
+		RetrieveActorAttributeToFloat("noise_alt_stddev", Description.Variations, 0.0f));
+	Gnss->SetLatitudeBias(
+		RetrieveActorAttributeToFloat("noise_lat_bias", Description.Variations, 0.0f));
+	Gnss->SetLongitudeBias(
+		RetrieveActorAttributeToFloat("noise_lon_bias", Description.Variations, 0.0f));
+	Gnss->SetAltitudeBias(
+		RetrieveActorAttributeToFloat("noise_alt_bias", Description.Variations, 0.0f));
+}
+
 void UActorBlueprintFunctionLibrary::SetIMU(
     const FActorDescription &Description,
     AInertialMeasurementUnit *IMU)

Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.h
@@ -16,6 +16,7 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include <util/ue-header-guard-end.h>
 
+#include "Autoware/Sensors/AutowareGnssSensor.h"
 #include "ActorBlueprintFunctionLibrary.generated.h"
 
 class AGnssSensor;
@@ -247,6 +248,7 @@ public:
   static void SetLidar(const FActorDescription &Description, FLidarDescription &Lidar);
 
   static void SetGnss(const FActorDescription &Description, AGnssSensor *Gnss);
+  static void SetAutowareGnss(const FActorDescription &Description, AAutowareGnssSensor *Gnss);
 
   static void SetIMU(const FActorDescription &Description, AInertialMeasurementUnit *IMU);
 

Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Autoware/Sensors/AutowareGnssSensor.h
@@ -8,28 +8,69 @@
 #include "AutowareGnssSensor.generated.h"
 
 UCLASS()
-class CARLA_API AAutowareGnssSensor : public AGnssSensor
+class CARLA_API AAutowareGnssSensor : public ASensor
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this actor's properties
 	AAutowareGnssSensor(const FObjectInitializer& ObjectInitializer);
 	
 	static FActorDefinition GetSensorDefinition();
+	void Set(const FActorDescription &ActorDescription);
 
-	// Temporary until AutowareGameModeBase is not valid to use. Later on swap to get this value from game mode.
+	void SetLatitudeDeviation(float Value);
+	void SetLongitudeDeviation(float Value);
+	void SetAltitudeDeviation(float Value);
+
+	void SetLatitudeBias(float Value);
+	void SetLongitudeBias(float Value);
+	void SetAltitudeBias(float Value);
+
+	float GetLatitudeDeviation() const;
+	float GetLongitudeDeviation() const;
+	float GetAltitudeDeviation() const;
+
+	float GetLatitudeBias() const;
+	float GetLongitudeBias() const;
+	float GetAltitudeBias() const;
+
+	double GetLatitudeValue() const;
+	double GetLongitudeValue() const;
+	double GetAltitudeValue() const;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsNoiseErrorEnabled() const;
+	
 	UFUNCTION(BlueprintCallable)
 	void LoadMgrsData();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
 	// virtual void PrePhysTick(float DeltaSeconds) override;
 	virtual void PostPhysTick(UWorld *World, ELevelTick TickType, float DeltaSeconds) override;
-	virtual carla::geom::GeoLocation ComputeGeoLocation() const override;
+	
+	UFUNCTION(BlueprintCallable)
+	void SetNoiseErrorActive(const bool bEnabled);
 
 private:
 	UPROPERTY()
 	TObjectPtr<UMgrsDataAsset> MgrsDataAsset = nullptr;
+	
+	carla::geom::GeoLocation CurrentGeoReference;
+
+	bool bApplyNoiseError = false;
+
+	float LatitudeDeviation;
+	float LongitudeDeviation;
+	float AltitudeDeviation;
+
+	float LatitudeBias;
+	float LongitudeBias;
+	float AltitudeBias;
+
+	double LatitudeValue;
+	double LongitudeValue;
+	double AltitudeValue;
 };

commit 3940be124614d5f3421119d5215d347ec3524d6b

fix: add null check for MgrsDataAsset in AutowareGnssSensor::PostPhysTick
Prevent SIGSEGV when MgrsDataAsset is not configured for the current map.
When MGRS data is unavailable, offset defaults to (0, 0, 0) instead of
crashing. This allows AutowareGnssSensor to work on maps without MGRS
data assets (e.g. Town10HD_Opt).

Bug introduced in 8791ce07b (2025-11-26).

Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Autoware/Sensors/AutowareGnssSensor.cpp
@@ -75,11 +75,13 @@ void AAutowareGnssSensor::PostPhysTick(UWorld* World, ELevelTick TickType, float
 		auto StreamId = carla::streaming::detail::token_type(GetToken()).get_stream_id();
 		AActor* ParentActor = GetAttachParentActor();
 		const FTransform sensor_world_transform = GetActorTransform();
-		double mgrs_offset_position[3] = {
-			static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.X),
-			static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.Y),
-			static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.Z),
-		};
+		double mgrs_offset_position[3] = {0.0, 0.0, 0.0};
+		if (MgrsDataAsset)
+		{
+			mgrs_offset_position[0] = static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.X);
+			mgrs_offset_position[1] = static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.Y);
+			mgrs_offset_position[2] = static_cast<double>(MgrsDataAsset->MgrsOffsetPosition.Z);
+		}
 		
 		if (ParentActor)
 		{