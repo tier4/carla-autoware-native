3ab73fd7673f3a5f49c33478fbc02cf86dbc2f2f
HOSOKAWA Ikuto <ikuto.hosokawa@tier4.jp>
2026-04-05 00:50:38 +0900

    fix(ros2): normalize DDS wire format type names in FastDDS TypeRegistry
    PubSubType::getName() returns DDS wire format names like
    "pkg::msg::dds_::Type_" but the TypeRegistry maps by ROS2 names
    "pkg::msg::Type". Add NormalizeDDSTypeName() to strip the "dds_::"
    prefix and trailing "_" before lookup. This fixes SEGFAULT when
    Autoware publishers/subscribers use PubSubType().getName() to
    obtain type names for the registry.

--- LibCarla/source/carla/ros2/dds/fastdds/FastDDSTypeRegistry.cpp
+++ LibCarla/source/carla/ros2/dds/fastdds/FastDDSTypeRegistry.cpp
@@ -8,6 +8,21 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <string>
+
+// Normalize DDS wire format type names to ROS2 names.
+// FastDDS PubSubType::getName() returns "pkg::msg::dds_::Type_" but
+// the registry uses ROS2 names "pkg::msg::Type".
+static std::string NormalizeDDSTypeName(const std::string& name) {
+    const std::string dds_infix = "::msg::dds_::";
+    auto pos = name.find(dds_infix);
+    if (pos == std::string::npos) return name;
+
+    std::string pkg = name.substr(0, pos);
+    std::string type = name.substr(pos + dds_infix.size());
+    if (!type.empty() && type.back() == '_') type.pop_back();
+    return pkg + "::msg::" + type;
+}
 
 // All PubSubTypes headers
 #include "carla/ros2/dds/fastdds/types/CameraInfoPubSubTypes.h"
@@ -60,119 +75,123 @@ namespace carla {
 namespace ros2 {
 
 efd::TypeSupport FastDDSTypeRegistry::Create(const std::string& type_name) {
+    // Normalize DDS wire format names (from PubSubType::getName()) to ROS2 names
+    const std::string normalized = NormalizeDDSTypeName(type_name);
+
     // sensor_msgs
-    if (type_name == "sensor_msgs::msg::CameraInfo")
+    if (normalized == "sensor_msgs::msg::CameraInfo")
         return efd::TypeSupport(new sensor_msgs::msg::CameraInfoPubSubType());
-    if (type_name == "sensor_msgs::msg::Image")
+    if (normalized == "sensor_msgs::msg::Image")
         return efd::TypeSupport(new sensor_msgs::msg::ImagePubSubType());
-    if (type_name == "sensor_msgs::msg::Imu")
+    if (normalized == "sensor_msgs::msg::Imu")
         return efd::TypeSupport(new sensor_msgs::msg::ImuPubSubType());
-    if (type_name == "sensor_msgs::msg::NavSatFix")
+    if (normalized == "sensor_msgs::msg::NavSatFix")
         return efd::TypeSupport(new sensor_msgs::msg::NavSatFixPubSubType());
-    if (type_name == "sensor_msgs::msg::NavSatStatus")
+    if (normalized == "sensor_msgs::msg::NavSatStatus")
         return efd::TypeSupport(new sensor_msgs::msg::NavSatStatusPubSubType());
-    if (type_name == "sensor_msgs::msg::PointCloud2")
+    if (normalized == "sensor_msgs::msg::PointCloud2")
         return efd::TypeSupport(new sensor_msgs::msg::PointCloud2PubSubType());
-    if (type_name == "sensor_msgs::msg::PointField")
+    if (normalized == "sensor_msgs::msg::PointField")
         return efd::TypeSupport(new sensor_msgs::msg::PointFieldPubSubType());
-    if (type_name == "sensor_msgs::msg::RegionOfInterest")
+    if (normalized == "sensor_msgs::msg::RegionOfInterest")
         return efd::TypeSupport(new sensor_msgs::msg::RegionOfInterestPubSubType());
 
     // std_msgs
-    if (type_name == "std_msgs::msg::Float32")
+    if (normalized == "std_msgs::msg::Float32")
         return efd::TypeSupport(new std_msgs::msg::Float32PubSubType());
-    if (type_name == "std_msgs::msg::Header")
+    if (normalized == "std_msgs::msg::Header")
         return efd::TypeSupport(new std_msgs::msg::HeaderPubSubType());
-    if (type_name == "std_msgs::msg::String")
+    if (normalized == "std_msgs::msg::String")
         return efd::TypeSupport(new std_msgs::msg::StringPubSubType());
 
     // geometry_msgs
-    if (type_name == "geometry_msgs::msg::Point")
+    if (normalized == "geometry_msgs::msg::Point")
         return efd::TypeSupport(new geometry_msgs::msg::PointPubSubType());
-    if (type_name == "geometry_msgs::msg::Point32")
+    if (normalized == "geometry_msgs::msg::Point32")
         return efd::TypeSupport(new geometry_msgs::msg::Point32PubSubType());
-    if (type_name == "geometry_msgs::msg::Pose")
+    if (normalized == "geometry_msgs::msg::Pose")
         return efd::TypeSupport(new geometry_msgs::msg::PosePubSubType());
-    if (type_name == "geometry_msgs::msg::PoseStamped")
+    if (normalized == "geometry_msgs::msg::PoseStamped")
         return efd::TypeSupport(new geometry_msgs::msg::PoseStampedPubSubType());
-    if (type_name == "geometry_msgs::msg::PoseWithCovariance")
+    if (normalized == "geometry_msgs::msg::PoseWithCovariance")
         return efd::TypeSupport(new geometry_msgs::msg::PoseWithCovariancePubSubType());
-    if (type_name == "geometry_msgs::msg::PoseWithCovarianceStamped")
+    if (normalized == "geometry_msgs::msg::PoseWithCovarianceStamped")
         return efd::TypeSupport(new geometry_msgs::msg::PoseWithCovarianceStampedPubSubType());
-    if (type_name == "geometry_msgs::msg::Quaternion")
+    if (normalized == "geometry_msgs::msg::Quaternion")
         return efd::TypeSupport(new geometry_msgs::msg::QuaternionPubSubType());
-    if (type_name == "geometry_msgs::msg::Transform")
+    if (normalized == "geometry_msgs::msg::Transform")
         return efd::TypeSupport(new geometry_msgs::msg::TransformPubSubType());
-    if (type_name == "geometry_msgs::msg::TransformStamped")
+    if (normalized == "geometry_msgs::msg::TransformStamped")
         return efd::TypeSupport(new geometry_msgs::msg::TransformStampedPubSubType());
-    if (type_name == "geometry_msgs::msg::Twist")
+    if (normalized == "geometry_msgs::msg::Twist")
         return efd::TypeSupport(new geometry_msgs::msg::TwistPubSubType());
-    if (type_name == "geometry_msgs::msg::TwistWithCovariance")
+    if (normalized == "geometry_msgs::msg::TwistWithCovariance")
         return efd::TypeSupport(new geometry_msgs::msg::TwistWithCovariancePubSubType());
-    if (type_name == "geometry_msgs::msg::Vector3")
+    if (normalized == "geometry_msgs::msg::Vector3")
         return efd::TypeSupport(new geometry_msgs::msg::Vector3PubSubType());
 
     // nav_msgs
-    if (type_name == "nav_msgs::msg::Odometry")
+    if (normalized == "nav_msgs::msg::Odometry")
         return efd::TypeSupport(new nav_msgs::msg::OdometryPubSubType());
 
     // rosgraph_msgs (namespace is 'rosgraph' in generated code)
-    if (type_name == "rosgraph_msgs::msg::Clock" || type_name == "rosgraph::msg::Clock")
+    if (normalized == "rosgraph_msgs::msg::Clock" || type_name == "rosgraph::msg::Clock")
         return efd::TypeSupport(new rosgraph::msg::ClockPubSubType());
 
     // builtin_interfaces
-    if (type_name == "builtin_interfaces::msg::Time")
+    if (normalized == "builtin_interfaces::msg::Time")
         return efd::TypeSupport(new builtin_interfaces::msg::TimePubSubType());
 
     // tf2_msgs
-    if (type_name == "tf2_msgs::msg::TF2Error")
+    if (normalized == "tf2_msgs::msg::TF2Error")
         return efd::TypeSupport(new tf2_msgs::msg::TF2ErrorPubSubType());
-    if (type_name == "tf2_msgs::msg::TFMessage")
+    if (normalized == "tf2_msgs::msg::TFMessage")
         return efd::TypeSupport(new tf2_msgs::msg::TFMessagePubSubType());
 
     // carla_msgs
-    if (type_name == "carla_msgs::msg::CarlaCollisionEvent")
+    if (normalized == "carla_msgs::msg::CarlaCollisionEvent")
         return efd::TypeSupport(new carla_msgs::msg::CarlaCollisionEventPubSubType());
-    if (type_name == "carla_msgs::msg::CarlaEgoVehicleControl")
+    if (normalized == "carla_msgs::msg::CarlaEgoVehicleControl")
         return efd::TypeSupport(new carla_msgs::msg::CarlaEgoVehicleControlPubSubType());
-    if (type_name == "carla_msgs::msg::LaneInvasionEvent")
+    if (normalized == "carla_msgs::msg::LaneInvasionEvent")
         return efd::TypeSupport(new carla_msgs::msg::LaneInvasionEventPubSubType());
 
     // autoware_control_msgs
-    if (type_name == "autoware_control_msgs::msg::Control")
+    if (normalized == "autoware_control_msgs::msg::Control")
         return efd::TypeSupport(new autoware_control_msgs::msg::ControlPubSubType());
-    if (type_name == "autoware_control_msgs::msg::Lateral")
+    if (normalized == "autoware_control_msgs::msg::Lateral")
         return efd::TypeSupport(new autoware_control_msgs::msg::LateralPubSubType());
-    if (type_name == "autoware_control_msgs::msg::Longitudinal")
+    if (normalized == "autoware_control_msgs::msg::Longitudinal")
         return efd::TypeSupport(new autoware_control_msgs::msg::LongitudinalPubSubType());
 
     // autoware_vehicle_msgs
-    if (type_name == "autoware_vehicle_msgs::msg::ControlModeReport")
+    if (normalized == "autoware_vehicle_msgs::msg::ControlModeReport")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::ControlModeReportPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::Engage")
+    if (normalized == "autoware_vehicle_msgs::msg::Engage")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::EngagePubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::GearCommand")
+    if (normalized == "autoware_vehicle_msgs::msg::GearCommand")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::GearCommandPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::GearReport")
+    if (normalized == "autoware_vehicle_msgs::msg::GearReport")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::GearReportPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::HazardLightsCommand")
+    if (normalized == "autoware_vehicle_msgs::msg::HazardLightsCommand")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::HazardLightsCommandPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::HazardLightsReport")
+    if (normalized == "autoware_vehicle_msgs::msg::HazardLightsReport")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::HazardLightsReportPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::SteeringReport")
+    if (normalized == "autoware_vehicle_msgs::msg::SteeringReport")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::SteeringReportPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::TurnIndicatorsCommand")
+    if (normalized == "autoware_vehicle_msgs::msg::TurnIndicatorsCommand")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::TurnIndicatorsCommandPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::TurnIndicatorsReport")
+    if (normalized == "autoware_vehicle_msgs::msg::TurnIndicatorsReport")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::TurnIndicatorsReportPubSubType());
-    if (type_name == "autoware_vehicle_msgs::msg::VelocityReport")
+    if (normalized == "autoware_vehicle_msgs::msg::VelocityReport")
         return efd::TypeSupport(new autoware_vehicle_msgs::msg::VelocityReportPubSubType());
 
     // tier4_vehicle_msgs
-    if (type_name == "tier4_vehicle_msgs::msg::VehicleEmergencyStamped")
+    if (normalized == "tier4_vehicle_msgs::msg::VehicleEmergencyStamped")
         return efd::TypeSupport(new tier4_vehicle_msgs::msg::VehicleEmergencyStampedPubSubType());
 
-    std::cerr << "FastDDSTypeRegistry::Create: unknown type '" << type_name << "'" << std::endl;
+    std::cerr << "FastDDSTypeRegistry::Create: unknown type '" << type_name
+              << "' (normalized: '" << normalized << "')" << std::endl;
     return efd::TypeSupport(nullptr);
 }
 
